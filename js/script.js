b275327b7e133bae57636e24bb2a704994049df4
Oghenedoro Ohwojivwiogor <francisohwos@gmail.com>
2023-10-13 15:53:34 +0100

    made some changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 const login = document.querySelector('.login');
+const search = document.getElementById('search-button');
+const f = document.getElementById('search-form');
+const q = document.getElementById('query')
+const google = 'https://www.google.com/search?q=site%3A+';
+const site = 'http://127.0.0.1:5506/pages/auction-page-all-items.html';
+
+//declaring the function that submit the search
+
+const submitted = (event) =>{
+    event.preventDefault();
+    const url = google + site + '+' + q.value;
+    const win = window.open(url , '_blank');
+    // win.focus();
+    
+}
+
+// search.addEventListener('submit', submitted);
+f.addEventListener('submit', submitted)
+alert('how are you')
 // login.style.display = '';
 const sign_in_btn = document.querySelector('.sign_in');
 const signIn = () =>{
@@ -10,7 +29,5 @@ const signIn = () =>{
     login.style.display = 'flex';
     details.style.display = 'none'
     more.style.display = 'none'
-
-
-}
-sign_in_btn.addEventListener('click',signIn)
+};
+sign_in_btn.addEventListener('click',signIn);
